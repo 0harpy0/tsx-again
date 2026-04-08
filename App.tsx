42543950a4f8c2f7c83a1cbcb6e7bb04a4b0c0ee
0harpy0 <160989826+0harpy0@users.noreply.github.com>
2026-04-08 12:21:01 -0300

    Add files via upload
    npx create-expo-app --template blank-typescript
     
    npx expo start --tunnel

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Banco } from './Banco/conf';
+import { useEffect } from 'react';
+
+
+
+export default function App() {
+  useEffect( () =>{
+      async function main() {
+        const db = await Banco();
+        
+      };
+
+
+
+  })
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
